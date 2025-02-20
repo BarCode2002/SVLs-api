5927c463fd9e320c747dd9b915d26fbab14c5ad6
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2025-02-20 22:12:39 +0100

    avances

--- src/mongo/mongo.service.ts
+++ src/mongo/mongo.service.ts
@@ -32,8 +32,8 @@ export class MongoService {
     if (!this.connection.db) {
       throw new Error('Database connection is not available');
     }
-    const translationsCollection = this.connection.db.collection<List>('lists');
-    const result = await translationsCollection.findOne({ type });
+    const listsCollection = this.connection.db.collection<List>('lists');
+    const result = await listsCollection.findOne({ type });
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     if (result) return result[type];
     else return result;
